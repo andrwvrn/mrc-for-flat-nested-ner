5e0ec6481be836310848f2d9d2da62016ab057bb
andrew <voroninandrewi@gmail.com>
2022-05-04 14:34:52 +0300

    added docstrings in tagger_span_f1 module

--- metrics/functional/tagger_span_f1.py
+++ metrics/functional/tagger_span_f1.py
@@ -6,12 +6,32 @@
 import torch
 import torch.nn.functional as F
 
+from typing import Dict, List, Tuple, Union
 
-def transform_predictions_to_labels(sequence_input_lst, wordpiece_mask, idx2label_map, input_type="logit"):
+
+def transform_predictions_to_labels(sequence_input_lst: torch.Tensor,
+                                    wordpiece_mask: torch.Tensor,
+                                    idx2label_map: Dict[int, str],
+                                    input_type: str = "logit") -> List[List[Union[int, str]]]:
     """
-    shape:
-        sequence_input_lst: [batch_size, seq_len, num_labels]
-        wordpiece_mask: [batch_size, seq_len, ]
+    Returns list of lists of labels derived from predictions
+
+    Args:
+        sequence_input_lst: torch.Tensor
+            tensor containing predictions, its shape is [batch_size, seq_len]
+            for 'label' input type, [batch_size, seq_len, num_labels] otherwise
+        wordpiece_mask: torch.Tensor, shape [batch_size, seq_len]
+            tensor containing mask with 1 for tokens that are the first part of
+            tokenized word and 0 for other tokens
+        idx2label_map: Dict[int, str]
+            mapping from tokens' indices in vocab to tokens
+        input_type: str
+            type of input provided in sequence_input_list. Possible input types
+            are ['logit', 'prob', 'label']
+
+    Returns:
+        output_label_sequence: List[List[Union[int, str]]]
+            List of lists of tokens' labels
     """
     wordpiece_mask = wordpiece_mask.detach().cpu().numpy().tolist()
 
@@ -41,7 +61,22 @@ def transform_predictions_to_labels(sequence_input_lst, wordpiece_mask, idx2labe
     return output_label_sequence
 
 
-def compute_tagger_span_f1(sequence_pred_lst, sequence_gold_lst):
+def compute_tagger_span_f1(sequence_pred_lst: List[List[Union[int, str]]],
+                           sequence_gold_lst: List[List[Union[int, str]]]) -> torch.Tensor:
+    """
+    Calculates TP, FP, FN rates for labels sequences
+
+    Args:
+        sequence_pred_lst: List[List[Union[int, str]]]
+            list containing lists of predicted labels
+        sequence_gold_lst: List[List[Union[int, str]]]
+            list containing lists of ground truth labels
+
+    Returns:
+        batch_confusion_matrix: torch.Tensor
+            tensor containing TP, FP, FN rates calculated on input labels
+            sequences
+    """
     sum_true_positive, sum_false_positive, sum_false_negative = 0, 0, 0
 
     for seq_pred_item, seq_gold_item in zip(sequence_pred_lst, sequence_gold_lst):
@@ -59,7 +94,20 @@ def compute_tagger_span_f1(sequence_pred_lst, sequence_gold_lst):
     return batch_confusion_matrix
 
 
-def count_confusion_matrix(pred_entities, gold_entities):
+def count_confusion_matrix(pred_entities: List[Union[int, str]],
+                           gold_entities: List[Union[int, str]]) -> Tuple[int, int, int]:
+    """
+    Returns TP, FP, FN rates for input labels sequences
+
+    Args:
+        pred_entities: List[Union[int, str]]
+            list containing predicted labels
+        gold_entities: List[Union[int, str]]
+            list containing ground truth labels
+
+    Returns:
+        tuple with three elements: TP, FP and FN rates for input sequences
+    """
     true_positive, false_positive, false_negative = 0, 0, 0
 
     for span_item in pred_entities:
@@ -75,10 +123,48 @@ def count_confusion_matrix(pred_entities, gold_entities):
     return true_positive, false_positive, false_negative
 
 
-def get_entity_from_bmes_lst(label_list):
-    """reuse the code block from
-        https://github.com/jiesutd/NCRFpp/blob/105a53a321eca9c1280037c473967858e01aaa43/utils/metric.py#L73
-        Many thanks to Jie Yang.
+def reverse_style(input_string: str) -> str:
+    """
+    Moves brackets from end to the beginning of the input_string
+
+    Args:
+        input_string: str
+            string with brackets
+
+    Returns:
+        output_string: str
+            string with brackets at the beginning
+
+    Examples:
+        >> input_string = 'abc[0,4]'
+        >> reverse_style(input_string)
+        >> '[0,4]abc'
+    """
+    target_position = input_string.index('[')
+    input_len = len(input_string)
+    output_string = input_string[target_position:input_len] + input_string[0:target_position]
+    return output_string
+
+
+def get_entity_from_bmes_lst(label_list: List[Union[int, str]]) -> List[str]:
+    """
+    Returns list of labels with their positions
+
+    Args:
+        label_list: List[Union[int, str]]
+            list containing bmes markup labels
+
+    Returns:
+        stand_matrix: List[str]
+            list containing strings with labels and their positions
+
+    Examples:
+        >> label_list = [-100, 'O', 'B-POS', 'E-POS', 'O']
+        >> get_entity_from_bmes_lst(label_list)
+        >> ['[2,3]POS']
+        >> label_list = ['O', 'S-PER', 'B-ORG', 'M-ORG', 'O']
+        >> get_entity_from_bmes_lst(label_list)
+        >> ['[1]PER', '[2,4]ORG']
     """
     list_len = len(label_list)
     begin_label = 'B-'
@@ -96,21 +182,21 @@ def get_entity_from_bmes_lst(label_list):
         else:
             continue
 
-        if begin_label in current_label:
+        if current_label.startswith(begin_label):
             if index_tag != '':
                 tag_list.append(whole_tag + ',' + str(i-1))
             whole_tag = current_label.replace(begin_label, "", 1) + '[' + str(i)
             index_tag = current_label.replace(begin_label, "", 1)
 
-        elif single_label in current_label:
+        elif current_label.startswith(single_label):
             if index_tag != '':
                 tag_list.append(whole_tag + ',' + str(i-1))
             whole_tag = current_label.replace(single_label, "", 1) + '[' + str(i)
             tag_list.append(whole_tag)
-            whole_tag = ""
-            index_tag = ""
+            whole_tag = ''
+            index_tag = ''
 
-        elif end_label in current_label:
+        elif current_label.startswith(end_label):
             if index_tag != '':
                 tag_list.append(whole_tag + ',' + str(i))
             whole_tag = ''
@@ -130,10 +216,3 @@ def get_entity_from_bmes_lst(label_list):
             stand_matrix.append(insert_list)
 
     return stand_matrix
-
-
-def reverse_style(input_string):
-    target_position = input_string.index('[')
-    input_len = len(input_string)
-    output_string = input_string[target_position:input_len] + input_string[0:target_position]
-    return output_string
